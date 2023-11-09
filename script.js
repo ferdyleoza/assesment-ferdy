bc5c1e6840f6fa8f5687a3c225e011ec7861e3ca
ferdyleoza <ferdyleoza123@gmail.com>
2023-11-09 18:33:52 +0700

    ass ferdy

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeImg(){
+    document.getElementById("Imgchange").src='img/img8.jpg'
+    document.getElementById("Imgchange1").src='img/img7.jpg'
+    document.getElementById("Imgchange2").src='img/img5.jpg'
+}
